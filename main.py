9a2a2305875889b09555a2411d20aeb5b512d4f5
andreJesusTorres <andre.torres@outlook.com.ar>
2023-07-04 16:51:25 -0300

    Prueba colaborador 2

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class App(customtkinter.CTk):
             style.theme_use("default")    
             style.configure("Treeview",
                             background="red",
-                            foreground="white",
+                            foreground="black",
                             rowheight=25,
                             fieldbackground="gray14",
                             bordercolor="#343638",
