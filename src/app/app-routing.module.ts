43568c77a728687960fd68ff946a84cddc65f1b3
andreJesusTorres <andre.torres@outlook.com.ar>
2023-10-09 23:25:59 -0300

    unico commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { AngularComponent } from './angular/angular.component';
+import { GitComponent } from './git/git.component';
+import { TerminalesComponent } from './terminales/terminales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'angular', component: AngularComponent },
+  { path: 'git', component: GitComponent },
+  { path: 'terminales', component: TerminalesComponent },
+  { path: "", redirectTo: "/inicio", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AngularComponent } from './angular/angular.component';
+import { GitComponent } from './git/git.component';
+import { TerminalesComponent } from './terminales/terminales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AngularComponent,
+    GitComponent,
+    TerminalesComponent
   ],
   imports: [
     BrowserModule,
